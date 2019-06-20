808c43bfcee0e451379dc9ea06667c5133d88fb1
devchavarri <51434371+devchavarri@users.noreply.github.com>
2019-06-20 18:59:08 -0500

    First commit
    Upload original file .

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<title>HTML5 Index Ready</title>
+
+    <!-- General Metatags -->
+	<meta name="description" content="">
+  	<link rel="canonical" href="" />
+    <!-- Facebook OpenGraph-->
+  	<meta property="og:locale" content="es_ES" />
+  	<meta property="og:title" content="" />
+  	<meta property="og:description" content="" />
+  	<meta property="og:url" content="" />
+  	<meta property="og:site_name" content="" />
+  	<meta property="fb:app_id" content="770148196451910"/>
+  	<meta property="og:image" content="" />
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/layout.css"/>	
+	<link rel="stylesheet" href="css/cover.css" />	
+	<link rel="stylesheet" href="fontisto/css/fontisto/fontisto.min.css"></i>
+    <!-- FAVICON -->
+	<link rel="icon" type="image/vnd.microsoft.icon" href="img/favicon.png" />
+
+</head>
+
+<body>
+
+</body>
+
+	<!-- Global Site Tag (gtag.js) - Google Analytics -->
+    <!--
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-106454339-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments)};
+      gtag('js', new Date());
+
+      gtag('config', 'UA-106454339-1');
+    </script>
+    -->
+
+</html>
